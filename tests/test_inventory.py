fcbdd7608adb332da05a718a91d6a1e8a5ae2940
Sarah Nadi <nadi@ualberta.ca>
2025-03-05 10:20:48 +0400

    Explain mocking vs. stubbing

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,4 +1,4 @@
-'''
+"""
 Some useful info:
 - A stub is used to provide predefined responses for method calls, but it does not verify how those methods were used. 
 It only controls the output of a method.
@@ -21,7 +21,7 @@ class GradeBookStub(GradeBook):
     def get_grades(self):
         # instead of communicating with DB, return fixed values
         return [1.0, 3.0, 2.7]
-'''
+"""
 
 import pytest
 from inventory_system.inventory import Inventory
@@ -30,56 +30,73 @@ from unittest.mock import MagicMock
 from inventory_system.database_service import DatabaseService
 from inventory_system.notification_service import NotificationService
 
+
 @pytest.fixture
 def mock_notification_service():
     """Fixture to provide a mocked notification service."""
     return MagicMock()
+
 
 @pytest.fixture
 def mock_database_service():
     """Fixture to provide a mocked database service."""
     return MagicMock()
 
+
 @pytest.fixture
 def inventory(mock_notification_service, mock_database_service):
     """Fixture to create an Inventory instance with mocked dependencies."""
     return Inventory(mock_notification_service, mock_database_service)
 
+
 @pytest.fixture
 def product():
     return Product(1, "Test Product", 10)
 
+
 def test_add_product_notexisting(inventory, mock_database_service, product):
-    mock_database_service.product_exists.return_value = False # stubbing
+    mock_database_service.product_exists.return_value = False  # stubbing
     inventory.add_product(product)
-    mock_database_service.save_product.assert_called_once_with(product) # mocking
+    mock_database_service.save_product.assert_called_once_with(product)  # mocking
     assert inventory.get_product_by_id(1) == product
 
-def test_add_product_existing(inventory, mock_database_service, product, mock_notification_service):
-    mock_database_service.product_exists.return_value = True # stubbing
-    inventory.add_product(product)
-    mock_database_service.save_product.assert_not_called() # mocking
-    assert inventory.get_product_by_id(1) is None
-    assert mock_notification_service.send_notification.called # mocking
 
-def test_update_quantity_above(inventory, mock_database_service, product, mock_notification_service):
-    mock_database_service.product_exists.return_value = False # stubbing
+def test_add_product_existing(
+    inventory, mock_database_service, product, mock_notification_service
+):
+    mock_database_service.product_exists.return_value = True  # stubbing
+    inventory.add_product(product)
+    mock_database_service.save_product.assert_not_called()  # mocking
+    assert inventory.get_product_by_id(1) is None
+    assert mock_notification_service.send_notification.called  # mocking
+
+
+def test_update_quantity_above(
+    inventory, mock_database_service, product, mock_notification_service
+):
+    mock_database_service.product_exists.return_value = False  # stubbing
     inventory.add_product(product)
     inventory.update_quantity(1, 10)
-    mock_database_service.save_product.assert_called_once # mocking
-    mock_notification_service.send_notification.assert_not_called() # mocking
+    mock_database_service.save_product.assert_called_once  # mocking
+    mock_notification_service.send_notification.assert_not_called()  # mocking
     assert product.get_quantity() == 10
-    
-def test_update_quantity_below(inventory, mock_database_service, product, mock_notification_service):
-    mock_database_service.product_exists.return_value = False # stubbing
+
+
+def test_update_quantity_below(
+    inventory, mock_database_service, product, mock_notification_service
+):
+    mock_database_service.product_exists.return_value = False  # stubbing
     inventory.add_product(product)
     inventory.update_quantity(1, 3)
-    mock_database_service.save_product.assert_called_once # mocking
-    mock_notification_service.send_notification.assert_called_once() # mocking
+    mock_database_service.save_product.assert_called_once  # mocking
+    mock_notification_service.send_notification.assert_called_once()  # mocking
     assert product.get_quantity() == 3
 
-def test_update_nonexisting(inventory, mock_database_service, product, mock_notification_service):
+
+def test_update_nonexisting(
+    inventory, mock_database_service, product, mock_notification_service
+):
     inventory.update_quantity(1, 3)
-    mock_database_service.save_product.assert_not_called()
-    mock_notification_service.send_notification.assert_not_called()
+    mock_database_service.save_product.assert_not_called()  # mocking
+    mock_notification_service.send_notification.assert_not_called()  # mocking
     assert inventory.get_product_by_id(1) is None
